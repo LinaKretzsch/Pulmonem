8b6789285c89197df4b32e01b4657cd6e638d1b6
Lina Kretzschmar <lina.kretzschmar@hotmail.de>
2021-03-27 21:28:53 -0400

    Added Cloud Background
    Added Cloud Background and set z-index to 0 to have clouds behind transparent scroll container

--- app/assets/javascript/three.js
+++ app/assets/javascript/three.js
@@ -1,4 +1,11 @@
-let scene, camera;
+let scene, camera, cloudParticles = [], container;
+
+container = document.getElementById('canvas');
+
+let threeDiv = document.createElement('threeDiv');
+threeDiv.style.zindex = 0;
+
+
 
 function init()
 {
@@ -9,33 +16,52 @@ function init()
   camera.rotation.y = -0.12;
   camera.rotation.z = 0.27;
 
-  let ambient = new THREE.AmbientLight(0x555555);
+  let ambient = new THREE.AmbientLight(0x25563);
   scene.add(ambient);
+
+  // Adding lights which adds colour
+  let directionalLight = new THREE.DirectionalLight(0xff8c19);
+  directionalLight.position.set(0,0,1);
+  scene.add(directionalLight);
+
+  let redPointLight = new THREE.PointLight(0x54d86c, 2, 450, 80.7);
+  redPointLight.position.set(100,300,100);
+  scene.add(redPointLight);
+
+  let orangePointLight   = new THREE.PointLight(0x36aca6, 4, 450, 40.7);
+  orangePointLight.position.set(300,300,100);
+  scene.add(orangePointLight);
+
+  let bluePointLight = new THREE.PointLight(0xcc6600, 10, 450, 10.7);
+  bluePointLight.position.set(200,300,100);
+  scene.add(bluePointLight);
+
 
   renderer = new THREE.WebGLRenderer();
   renderer.setSize(window.innerWidth, window.innerHeight);
-  scene.fog = new THREE.FogExp2(0x03544e, 0.001);
+  scene.fog = new THREE.FogExp2(0x010003, 0.001);
   renderer.setClearColor(scene.fog.color);
-  document.body.appendChild(renderer.domElement);
+  container.appendChild(renderer.domElement);
 
   let loader = new THREE.TextureLoader();
-  loader.load("smoke.png", function(texture){
+  loader.load("assets/javascript/smoke.png", function(texture){
     cloudGeo = new THREE.PlaneBufferGeometry(500,500);
-    cloudMaterial = new THREE.MashLambertMaterial({
+    cloudMaterial = new THREE.MeshLambertMaterial({
       map:texture,
       transparent:true
     });
-    for(leti = 0; i< 50; i++){
-      let cloud = new THREE.Mesh(cloudGEO, cloudMaterial);
+    for(let i = 0; i< 20; i++){
+      let cloud = new THREE.Mesh(cloudGeo, cloudMaterial);
       cloud.position.set(
-        Math.random()*800-400,
+        Math.random()*1000-400,
         500,
         Math.random()*500-500
       );
       cloud.rotation.x = 1.16;
       cloud.rotation.y = -0.12;
       cloud.rotation.z = Math.random()*2*Math.PI;
-      cloud.material.opacity = 0.55;
+      cloud.material.opacity = 0.10;
+      cloudParticles.push(cloud);
       scene.add(cloud);
       }
   })
@@ -43,6 +69,9 @@ function init()
   render();
 }
 function render() {
+  cloudParticles.forEach(p=> {
+    p.rotation.z -= 0.001
+  });
   renderer.render(scene,camera);
   requestAnimationFrame(render);
 }
